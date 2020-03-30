be154ea871d4c441881536c8d82b44e0e93be360
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-03-29 19:13:12 -0500

    📝 added coverage badge (#1)
    * added codecov
    
    * :pencil: added codecov badge to readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WebHooks
 
-[![Node CI](https://github.com/bmiddha/webhooks/workflows/Node%20CI/badge.svg)](https://github.com/bmiddha/webhooks/actions?query=workflow%3A%22Node+CI%22) [![NPM Publish](https://github.com/bmiddha/webhooks/workflows/NPM%20Publish/badge.svg)](https://github.com/bmiddha/webhooks/actions?query=workflow%3A%22NPM+Publish%22) 
+[![Node CI](https://github.com/bmiddha/webhooks/workflows/Node%20CI/badge.svg)](https://github.com/bmiddha/webhooks/actions?query=workflow%3A%22Node+CI%22) [![NPM Publish](https://github.com/bmiddha/webhooks/workflows/NPM%20Publish/badge.svg)](https://github.com/bmiddha/webhooks/actions?query=workflow%3A%22NPM+Publish%22) [![codecov](https://codecov.io/gh/bmiddha/webhooks/branch/master/graph/badge.svg)](https://codecov.io/gh/bmiddha/webhooks)
 
 Easy-to-use, object oriented WebHooks library written in TypeScript.
 
--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,11 @@ type RequestFunction = (shortName: string, jsonData: {}, headersData?: {}) => Pr
 export class WebHooks {
     #redisClient: Redis;
     #emitter: EventEmitter;
-    #functions: HashTable<RequestFunction>;
+    #requestFunctions: HashTable<RequestFunction>;
     constructor({ redisClient }: Options) {
         this.#redisClient = redisClient;
         this.#emitter = new EventEmitter();
-        this.#functions = {};
+        this.#requestFunctions = {};
         this.#setListeners();
     }
     
@@ -34,8 +34,8 @@ export class WebHooks {
             const urls: string[] = JSON.parse(await this.#redisClient.get(key));
             urls.forEach(url => {
                 const encUrl = crypto.createHash('md5').update(url).digest('hex');
-                this.#functions[encUrl] = this.#getRequestFunction(url);
-                this.#emitter.on(key, this.#functions[encUrl]);
+                this.#requestFunctions[encUrl] = this.#getRequestFunction(url);
+                this.#emitter.on(key, this.#requestFunctions[encUrl]);
             });
         });
     };
@@ -79,8 +79,8 @@ export class WebHooks {
         if (urls.indexOf(url) === -1) {
             urls.push(url);
             const encUrl = crypto.createHash('md5').update(url).digest('hex');
-            this.#functions[encUrl] = this.#getRequestFunction(url);
-            this.#emitter.on(shortName, this.#functions[encUrl]);
+            this.#requestFunctions[encUrl] = this.#getRequestFunction(url);
+            this.#emitter.on(shortName, this.#requestFunctions[encUrl]);
             await this.#redisClient.set(shortName, JSON.stringify(urls));
         } else {
             throw new Error(`URL(${url}) already exists for shortName(${shortName}).`);
@@ -90,8 +90,8 @@ export class WebHooks {
         if (url) {
             await this.#removeUrlFromShortName(shortName, url);
             const urlKey = crypto.createHash('md5').update(url).digest('hex');
-            this.#emitter.removeListener(shortName, this.#functions[urlKey]);
-            delete this.#functions[urlKey];
+            this.#emitter.removeListener(shortName, this.#requestFunctions[urlKey]);
+            delete this.#requestFunctions[urlKey];
         } else {
             if (!(await this.#redisClient.exists(shortName)))
                 throw new Error(`ShortName(${shortName}) not found while removing.`);
@@ -99,7 +99,7 @@ export class WebHooks {
             const urls: string[] = JSON.parse(await this.#redisClient.get(shortName));
             urls.forEach(url => {
                 const urlKey = crypto.createHash('md5').update(url).digest('hex');
-                delete this.#functions[urlKey];
+                delete this.#requestFunctions[urlKey];
             });
             await this.#removeShortName(shortName);
         }
@@ -119,8 +119,8 @@ export class WebHooks {
             throw new Error(`ShortName(${shortName}) not found while removing.`);
         return await this.#redisClient.get(shortName);
     };
-    get listener(): HashTable<RequestFunction> {
-        return this.#functions;
+    get requestFunctions(): HashTable<RequestFunction> {
+        return this.#requestFunctions;
     }
     get emitter(): EventEmitter {
         return this.#emitter;
